8d4d1fab1672d6f60b2c6337c345fee60075766f
MMundanu <mmundanu@yahoo.com>
2023-05-18 19:30:32 -0300

    vistas

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,16 +1,44 @@
 const db = require('../database/models');
+const { Sequelize } = require("../database/models");
+
 
 module.exports = {
     index: (req, res) => {
         
         
-        const allProducts = db.Product.findAll();
+        let allProducts = db.Product.findAll({
+            includes:[{
+                association: 'category',
+                atributtes: ['id', 'name']
+            },
+        {
+            association: 'images'
+        }]
+        });
+
+        let categories = db.Category.findAll()
+
+        let productsRandom = db.Product.findAll({
+            limit: 4,
+            order: Sequelize.literal('rand()'),
+            includes:[{
+                association: 'category',
+                atributtes: ['id', 'name']
+            },
+        {
+            association: 'images'
+        }]
+        })
 
         
 
-
-        .then(products => {
-             res.send( products )
+        Promise.all([allProducts,categories,productsRandom])
+        .then(([allProducts, categories,productsRandom]) => {
+            return res.render('index', 
+            {   allProducts,
+                categories,
+                productsRandom
+            } )
         })
         .catch(error => console.log(error))
     }
